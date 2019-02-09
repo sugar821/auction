1ea52fb3352a0b2747b7b2bb49d9d863e613b0a9
sugar821 <aug.sugar@gmail.com>
2019-02-09 16:37:23 +0900

    add create/new controler

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,28 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
   
+  def new
+    @item=Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    # 引数としてメソッドを渡すと先にメソッド内を実行してくれる
+    @item.save
+    redirect_to @item
+  end
+
+  private
+  #どのカラムに対して更新許可を出すか
+  # ストロングパラメータの作成
+    def item_params
+      params.require(:item).permit(:name,
+                                    :price, 
+                                    :seller,
+                                    :description,
+                                    :email,
+                                    :image_url)
+    end
+
+
 end
